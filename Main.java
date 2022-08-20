2b1ad789883daecd972c546f01fc64a7c29581cb
linkeshvaranr <gokulrockz637g@gmail.com>
2022-08-20 11:46:29 +0530

    new line added

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String [] args){
-        System.out.println("Hi");
+        System.out.println("Gokul");
+        System.out.println("Hello");
 
     }
 
